67356fec7eaa8ee3b7378fbe6f7f0e9d29499228
Paulo Santos <paulobrasec@gmail.com>
2018-08-03 01:41:42 -0300

    Initial Commit

---
+++ Gerador de Números/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gerador de Números
+//
+//  Created by Paulo Cesar on 03/08/2018.
+//  Copyright © 2018 Paulo Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
